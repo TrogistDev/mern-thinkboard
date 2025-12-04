4dda955b18e755a5b0735f866dd0325911b22f8c
trogist15496-del <trogist15496@gmail.com>
2025-12-04 19:19:46 +0100

    path implementing

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,14 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path"
 dotenv.config();
 const app = express();
+const __dirname = path.resolve()
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors({ origin: "http://localhost:5173" }));
 
-app.use(cors({ origin: "http://localhost:5173" }));
+}
 app.use(express.json());
 app.use(rateLimiter);
 /* app.use((req,res,next)=> {
@@ -16,7 +20,12 @@ app.use(rateLimiter);
     }) */
 app.use("/api/notes", notesRoutes);
 // middleware
-
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+app.get("*", (req,res) => {
+  res.sendFile(path.join(__dirname,"../frontend","dist", "index.html"))
+})
+}
 connectDB().then(() => {
   app.listen(process.env.PORT, () => {
     console.log("server started on PORT: 5001");
