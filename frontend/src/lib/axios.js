e8c4307929246abc5cc768239e8fbb5e9f354f57
trogist15496-del <trogist15496@gmail.com>
2025-12-04 21:41:43 +0100

    backend connection fixing 12

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,7 @@
-import axios from "axios";
+import axios from "axios"
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/notes"
+const api = axios.create(
+    {baseURL: BASE_URL,}
+)
 
-const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL || "http://localhost:5001/api",
-});
-
-export default api;
+export default api
