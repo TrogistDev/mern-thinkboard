7624120f6b2001f0e7ba86e2a48ffa0425ed597a
trogist15496-del <trogist15496@gmail.com>
2025-12-04 21:32:09 +0100

    backend connection fixing 10

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 const __dirname = path.resolve()
 if(process.env.NODE_ENV !== "production"){
-  app.use(cors({ origin: "http://localhost:5173", "https://mern-thinkboard-api.onrender.com/api" }));
+  app.use(cors({ origin: ["http://localhost:5173", "https://mern-thinkboard-api.onrender.com/app", "https://mern-thinkboard-74i2.onrender.com/"] }));
 
 }
 app.use(express.json());
